3331f86559833d1aa1dc197762358a3275f48f59
Mark Strendin <mark.strendin@lskysd.ca>
2016-09-07 11:46:41 -0600

    Initial commit

---
+++ LSKYGuestAccountControl/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LSKYGuestAccountControl
+{
+    public static class Settings
+    {
+        public static List<string> SecurityGroupsAllowed
+        {
+            get
+            {
+                try
+                {
+                    return System.Configuration.ConfigurationManager.AppSettings["allowed_ad_security_groups"].ToString().Split(';').ToList();
+                }
+                catch
+                {
+                    return new List<string>();
+                }
+            }
+        }
+    }
+}
